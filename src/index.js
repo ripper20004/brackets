9f91839e5d715d9944ed538aadbbe5f1e6cd8c81
ripper20004 <sashakarosnik@gmail.com>
2023-07-24 19:02:18 +0400

    feat: implement bracket function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+    const openingBrackets = new Set(bracketsConfig.map(pair => pair[0]));
+    const closingBrackets = new Set(bracketsConfig.map(pair => pair[1]));
+    const matchingBrackets = Object.fromEntries(bracketsConfig);
+
+    for (const char of str) {
+        if (openingBrackets.has(char)) {
+            if (closingBrackets.has(char) && stack[stack.length - 1] === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        } else if (closingBrackets.has(char)) {
+            if (stack.length === 0 || matchingBrackets[stack[stack.length - 1]] !== char) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+
+    return stack.length === 0;
 }
